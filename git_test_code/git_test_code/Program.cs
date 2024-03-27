7a6908adac041caf534011e8bfd0ba6bf7fdb0e1
Gulácsi Levente <gulacsilevente7@gmail.com>
2024-03-27 16:16:54 +0100

    empty console project

---
+++ git_test_code/git_test_code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace git_test_code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Kérek egy számot!: ");
+            //int felhasznaloSzam = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= felhasznaloSzam; i++)
+            //{
+            //    if (i == felhasznaloSzam)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //    else
+            //    {
+            //        Console.Write(i + ", ");
+            //    }
+
+            //}
+        }
+    }
+}
